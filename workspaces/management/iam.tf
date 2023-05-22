1f44cbb517b73e841660fa15c1b1200e12ee85f9
Sean Herman <seanjh@hey.com>
2023-05-21 23:18:48 -0400

    ok

---
+++ workspaces/management/iam.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = ["6938FD4D98BAB03FAADB97B34396831E3780AEA1"]
+}
